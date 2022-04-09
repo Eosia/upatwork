13c77b73f056f74bec6d49bd03ba968a7aaf2782
Eosia <kevin.eosiaweb@gmail.com>
2022-04-09 23:58:54 +0200

    Ajout model level

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('level_id')->constrained();
+            $table->foreignId('level_id')->default(1)->constrained();
             $table->foreignId('role_id')->constrained();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -3,8 +3,8 @@
     <input class="bg-gray-100 text-gray-900 border-2 px-3 py-2 w-64 rounded-full focus:outline-none placeholder-gray-500"
            placeholder="&#x1F50D; Rechercher un job ..."
            wire:model="query"
-           wire:keydown.arrow-down="incrementIndex"
-           wire:keydown.arrow-up="decrementIndex"
+           wire:keydown.arrow-down.prevent="incrementIndex"
+           wire:keydown.arrow-up.prevent="decrementIndex"
     />
 
     <div class="absolute bg-gray-200 text-md w-64 mt-1 border rounded">
